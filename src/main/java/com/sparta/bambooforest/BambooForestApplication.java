d63edc642231252d092f3489a4c820e9cc6bb82b
SoMin <thals0107@naver.com>
2023-03-17 21:21:05 +0900

    Initial commit

---
+++ src/main/java/com/sparta/bambooforest/BambooForestApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.bambooforest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BambooForestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BambooForestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/bambooforest/BambooForestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.bambooforest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BambooForestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
