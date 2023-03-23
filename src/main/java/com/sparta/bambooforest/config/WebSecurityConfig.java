b63e2a468e1a20f501ef032f678bf3b74fd04401
SoMin <thals0107@naver.com>
2023-03-24 00:04:10 +0900

    deploy: front 배포

--- src/main/java/com/sparta/bambooforest/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/bambooforest/config/WebSecurityConfig.java
@@ -74,7 +74,7 @@ public class WebSecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         // 사전에 약속된 출처를 명시
-        config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin("http://hanghae99-miniproject.s3-website.ap-northeast-2.amazonaws.com");
         //config.addAllowedOrigin("http://charleybucket.s3-website.ap-northeast-2.amazonaws.com");
 
         // 특정 헤더를 클라이언트 측에서 사용할 수 있게 지정
