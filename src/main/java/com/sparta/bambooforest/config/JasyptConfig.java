754106aacd68aa0a306baeb21965b36ec1aa4020
SoMin <thals0107@naver.com>
2023-03-23 14:00:18 +0900

    refactoring: JasyptConfig.java 삭제

--- src/main/java/com/sparta/bambooforest/config/JasyptConfig.java
+++ src/main/java/com/sparta/bambooforest/config/JasyptConfig.java
@@ -1,26 +1,26 @@
-package com.sparta.bambooforest.config;
-
-import org.jasypt.encryption.StringEncryptor;
-import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
-import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-// application.properties 파일 암호화
-@Configuration
-public class JasyptConfig {
-
-	@Value("${jasypt.password}")
-	private String encryptKey;
-	@Bean(name = "jasyptStringEncryptor")
-	public StringEncryptor stringEncryptor() {
-		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-		config.setPassword(encryptKey);
-		config.setAlgorithm("PBEWithMD5AndDES");
-		config.setPoolSize("1");
-		encryptor.setConfig(config);
-		return encryptor;
-	}
-}
+//package com.sparta.bambooforest.config;
+//
+//import org.jasypt.encryption.StringEncryptor;
+//import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+//import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//// application.properties 파일 암호화
+//@Configuration
+//public class JasyptConfig {
+//
+//	@Value("${jasypt.password}")
+//	private String encryptKey;
+//	@Bean(name = "jasyptStringEncryptor")
+//	public StringEncryptor stringEncryptor() {
+//		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+//		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+//		config.setPassword(encryptKey);
+//		config.setAlgorithm("PBEWithMD5AndDES");
+//		config.setPoolSize("1");
+//		encryptor.setConfig(config);
+//		return encryptor;
+//	}
+//}
